af4ae678b701925f65c63fe67ddb4ff3133214ce
George Thomas <georgefsthomas@gmail.com>
2024-09-14 13:41:24 +0100

    Remove top-level margin
    This is annoying and not very compositional. In Firefox at least, it can appear as a zero margin then expand suddenly when padding is added to the inner element.

--- src/Prettyprinter/Lucid.hs
+++ src/Prettyprinter/Lucid.hs
@@ -2,6 +2,7 @@ module Prettyprinter.Lucid (renderHtml) where
 
 import qualified Data.Text as T
 import Lucid (Html, ToHtml (toHtml), br_, pre_)
+import Lucid.Base (makeAttribute)
 import Prettyprinter.Render.Util.SimpleDocTree (SimpleDocTree (..))
 
 renderHtml :: SimpleDocTree (Html () -> Html ()) -> Html ()
@@ -13,4 +14,4 @@ renderHtml =
             STLine i -> br_ [] >> toHtml (T.replicate i $ T.singleton ' ')
             STAnn ann content -> ann $ go content
             STConcat contents -> foldMap go contents
-     in pre_ . go
+     in pre_ [makeAttribute "style" "margin: 0"] . go
